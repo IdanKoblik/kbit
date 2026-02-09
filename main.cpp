122fd642b6582268a062e056df68d399a98d06d3
IdanKoblik <me@idank.dev>
2026-02-09 16:08:29 +0200

    Small code changes
    Changes to be committed:
       modified:   decoder.cpp
       modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include "decoder.h"
 #include "command_factory.h"
 #include <memory>
 #include <iostream>
+#include <vector>
 
 static const std::vector<std::string> art = {
     "----    ---- -----------  --------  ------------",
@@ -15,11 +15,11 @@ static const std::vector<std::string> art = {
 };
 
 int main(int argc, char** argv) {
-    for (const auto& line : art) 
+    for (const std::string& line : art)
         std::cout << line << std::endl;
-    
+
     std::cout << "\n";
-    
+
     if (argc == 3) {
         std::unique_ptr<Command> cmd = get_command(argv[1]);
         if (cmd) {
